25d3684f92447af3a4921dd0dc90a1c10e692cb1
Dang Quang <quangmanuel@gmail.com>
2021-10-26 14:46:46 +0700

    them file nhac

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.use(express.static('public'))
 app.use("/css", express.static(__dirname + 'public/css'))
 app.use("/js", express.static(__dirname + 'public/js'))
 app.use("/images", express.static(__dirname + 'public/images'))
+app.use("/music", express.static(__dirname + 'public/music'))
+
 
 // Setting the view engine
 app.set("view engine", 'ejs');
