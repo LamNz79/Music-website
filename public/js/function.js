66fa577d3c122a8ccf9f6566a0f82477414aca3b
Dang Quang <quangmanuel@gmail.com>
2021-10-27 21:26:25 +0700

    fix navbar audio

--- public/js/function.js
+++ public/js/function.js
@@ -22,8 +22,8 @@ function getLogin() {
 
     }, (data, status) => {
         alert(JSON.stringify(data))
-        if (data.length === 0 || data === 'empty' || data === '') {
-            if (name === data[0].name && password === data[0].pass) {
+        if (data.length == 0 || data == 'empty' || data == '') {
+            if (name == data[0].name && password == data[0].pass) {
                 localStorage.setItem("user", JSON.stringify(data))
                 var objUser = JSON.parse(localStorage.getItem("user"))
                 $("#icon-settings").css(style = `text-align: center; background-image: url(${objUser[0]}); background-size: cover;`
